f4bd48990cd016ee22feec5a595dbef5678f6a7a
sachin_sde <ssachin.s@myclnq.co.in>
2025-02-15 16:58:47 +0530

    footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -28,32 +28,48 @@ function HomePage() {
               <h1 className="heading text-center capitalize">
                 All the SpaceX data in One Place
               </h1>
-            </article>
-            <article>
-              <h2>Headquarter</h2>
-              <ul>
-                <li>{company.headquarters?.address}</li>
-                <li>{company.headquarters?.city}</li>
-                <li>{company.headquarters?.state}</li>
-              </ul>
-            </article>
-            <article>
-              <h2>Headquarter</h2>
-              <ul>
-                <li>
-                  <a href={company.links?.website}>Website</a>
-                </li>
-                <li>
-                  <a href={company.links?.flickr}>flickr</a>
-                </li>
-                <li>
-                  <a href={company.links?.twitter}>twitter</a>
-                </li>
-                <li>
-                  <a href={company.links?.elon_twitter}>elon_twitter</a>
-                </li>
-              </ul>
-            </article>
+            <div className="grid grid-cols gap-5 md:grid-cols-2 lg:grid-cols-3 max-w-4xl mx-auto">
+              <article>
+                <h2>About</h2>
+                <ul className="text-sm opacity-75">
+                  <li>{company.name} was founded by</li>
+                  <li>{company.founder}in the year</li>
+                  <li>{company.founded}.</li>
+                  <li>It has {company.employees} employees,</li>
+                  <li>{company.vehicles} vehicles,</li>
+                  <li>{company.launch_sites} launch sites</li>
+                  <li> and {company.test_sites} test sites and </li>
+                  <li>is valued at {company.valuation.toLocaleString()} B</li>
+                </ul>
+              </article>
+              <article>
+                <h2>Headquarter</h2>
+                <ul className="text-sm opacity-75">
+                  <li>{company.headquarters?.address}</li>
+                  <li>{company.headquarters?.city}</li>
+                  <li>{company.headquarters?.state}</li>
+                </ul>
+              </article>
+              <article>
+                <h2>Useful Links</h2>
+                <ul className="text-sm opacity-75">
+                  <li>
+                    <a href={company.links?.website}>Website</a>
+                  </li>
+                  <li>
+                    <a href={company.links?.flickr}>flickr</a>
+                  </li>
+                  <li>
+                    <a href={company.links?.twitter}>twitter</a>
+                  </li>
+                  <li>
+                    <a href={company.links?.elon_twitter}>elon_twitter</a>
+                  </li>
+                </ul>
+              </article>
+            </div>
+            <p>{company.summary}</p>
+             </article>
           </div>
         </section>
       )}
