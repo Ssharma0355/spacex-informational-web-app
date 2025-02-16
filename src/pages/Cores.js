c162dd67bffdf3238edae38b8c82a8cb1ba14810
sachin_sde <ssachin.s@myclnq.co.in>
2025-02-16 17:07:01 +0530

    cores

--- src/pages/Cores.js
+++ src/pages/Cores.js
@@ -15,14 +15,22 @@ function Cores() {
       <section className="py-32">
         <h1 className="heading text-center mb-10">Cores</h1>
         <div className="max-width grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-4xl mx-auto px-5">
-          {cores.map(({ block, reuse_count }) => (
-            <artile key={block}>
-              <ul>
-                <li>{reuse_count}</li>
-                <li>Sample</li>
-              </ul>
-            </artile>
-          ))}
+          {cores.map(
+            ({
+              id,
+              status,
+              last_update,
+              reuse_count,
+              asds_landings,
+              rtls_landings,
+              serial,
+              launches,
+            }) => (
+              <artile key={id}>
+                <h1>{serial}</h1>
+              </artile>
+            )
+          )}
         </div>
       </section>
     </>
