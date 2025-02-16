4ef3394ec18f0caadd95f03022aa75afa2ed83e4
sachin_sde <ssachin.s@myclnq.co.in>
2025-02-16 19:29:29 +0530

    elonmusk

--- src/components/LoadingState.js
+++ src/components/LoadingState.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 function LoadingState() {
   return (
- <div className="spinner">
-    <article>
-    </article>
-    <p>It's Loading</p>
- </div>
-  )
+    <div className="spinner">
+      <article></article>
+      <p>Are Elon Musk's APIs fast? It's still loading.</p>
+    </div>
+  );
 }
 
 export default LoadingState
